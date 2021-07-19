7542c928d0262ef7d6f8e7b0c449ee65ba78c931
Raj Prajapati <rajce604@gmail.com>
2021-07-19 15:41:04 +0530

    DB changed.

--- busy-chaincode-api/config.js
+++ busy-chaincode-api/config.js
@@ -2,5 +2,7 @@ module.exports = {
   ENV: process.env.NODE_ENV || "development",
   PORT: process.env.PORT || "3000",
   URL: process.env.BASE_URL || "http://localhost:3000",
-  MONGODB_URI: process.env.MONGODB_URI || "mongodb://85.255.4.240:27017/busy",
+  MONGODB_URI:
+    process.env.MONGODB_URI ||
+    "mongodb://busyadmin:BusyChain123@85.255.4.240:27017/busy?authSource=admin",
 };
