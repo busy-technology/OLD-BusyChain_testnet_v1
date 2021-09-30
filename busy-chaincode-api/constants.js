86f100e8761aa4c804f9716554d58461df0fcc2e
Busy <78642145+busy-technology@users.noreply.github.com>
2021-09-30 15:30:14 +0200

    Update constants.js

--- busy-chaincode-api/constants.js
+++ busy-chaincode-api/constants.js
@@ -2,7 +2,7 @@ module.exports = Object.freeze({
     SEND_MESSAGE_AMOUNT: '1000000000000000000',
     DOMAIN_NAME_USER: 'busy.technology',
     DOMAIN_NAME_ADMIN: 'busy.admins',
-    API_KEY_USER: 'a1b2c33d4e5f6g7h8i9jakblc',
-    API_KEY_ADMIN: 'hckch874867487njkbjvw89797',
+    API_KEY_USER: '********************',
+    API_KEY_ADMIN: '********************',
     BUSY_TOKEN: 'BUSY',
 });
